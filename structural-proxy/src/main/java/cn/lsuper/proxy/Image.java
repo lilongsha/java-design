2ba4414d8f3b3218acff4b0937ba689fe3c36785
lilongsha@163.com <lilongsha@163.com>
2022-01-06 22:59:20 +0800

    feat: 代理模式

---
+++ structural-proxy/src/main/java/cn/lsuper/proxy/Image.java
@@ -0,0 +1,11 @@
+package cn.lsuper.proxy;
+
+/**
+ * @author ALGiii
+ */
+public interface Image {
+    /**
+     * 输出图形
+     */
+    void display();
+}
---
+++ structural-proxy/src/main/java/cn/lsuper/proxy/RealImage.java
@@ -0,0 +1,22 @@
+package cn.lsuper.proxy;
+
+/**
+ * @author ALGiii
+ */
+public class RealImage implements Image{
+    private final String fileName;
+
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadFromDisk(fileName);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + fileName);
+    }
+
+    void loadFromDisk(String fileName) {
+        System.out.println("Loading " + fileName);
+    }
+}
